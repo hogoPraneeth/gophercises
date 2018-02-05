9b22458e860e788080553197dee9977a635ef824
praneeth2k3 <praneeth2k3@gmail.com>
2018-02-05 13:55:17 -0800

    added command line options
    Added command line options

--- src/main/main.go
+++ src/main/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"quiz1"
 )
 
 
 func main() {
-	fmt.Println("Hello World\n")
-	quiz1.Run()
+	// defualt
+	config := os.Getenv("GOPATH") + "config/problems.csv"
+	if len(os.Args) > 3 && os.Args[2] == "-c" {
+		config = os.Args[3]
+	}
+	switch os.Args[1] {
+	case "-h":
+		fmt.Println("ARGUMENTS::\n\t -h : HELP\n\t 1: Quiz1")
+		break
+	case "-1":
+		quiz1.Run()
+		break
+	default:
+		fmt.Println("USAGE: ARGUMENTS::\n\t -h : HELP\n\t 1: Quiz1")
+	}
+	fmt.Println(config)
 }

8e421a7645eda6731a3c44c26aaf390453a6c247
praneeth2k3 <praneeth2k3@gmail.com>
2018-02-05 16:47:58 -0800

    csv read
    csv read. incomplete

--- src/quiz1/quiz.go
+++ src/quiz1/quiz.go
@@ -2,9 +2,26 @@ package quiz1
 
 import (
 	"fmt"
+	"encoding/csv"
+	"strings"
+	"io"
+	"log"
+	"os"
 )
 
 func Run() {
-	fmt.Println("hellp from quiz")
+	fmt.Println("hello from quiz")
+	file := os.Getenv("GOPATH") + "/config/problems.csv"
+	r := csv.NewReader(strings.NewReader(file))
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		fmt.Println(record)
+	}
 }
